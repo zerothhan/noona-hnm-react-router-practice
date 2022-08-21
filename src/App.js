f2ce2304e3a84e087e32439a17f7a0395552d703
zerothhan <zerothhan@gmail.com>
2022-08-21 20:46:11 +0900

    2. 라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
 
+// 1. 전체 상품 페이지, 로그인 페이지, 상품 상세 페이지
+// 2. 전체 상품 페이지 : 전체 상품을 볼 수 있다.
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+// 3. 상품 상세를 눌렀으나 로그인이 안되어 있을 경우에는 로그인 페이지가 먼저 나온다.
+// 4. 로그인이 되어 있을 경우에는 상품 상세 페이지를 볼 수 있다.
+// 5. 로그아웃 버튼을 누르면 로그아웃이 된다.
+// 5. 로그아웃이 되면 상품 상세 페이지를 볼 수 없다. 다시 로그인 페이지가 보인다.
+// 6. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
+// 7. 상품을 검색할 수 있다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
     </div>
   );
 }
