d25c512073f221c09c9f8ac95c5587c3032d7dda
Juhwa Hwang <zerothhan@gmail.com>
2022-08-23 10:30:15 +0900

    12. 상품 검색하기 2탄

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProductCard from "../component/ProductCard";
+import { useSearchParams } from "react-router-dom";
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
 
   const getProducts = async () => {
-    let url = `http://localhost:3004/products`;
+    let searchQuery = query.get("q") || "";
+    console.log(searchQuery);
+    let url = `http://localhost:3004/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
 
@@ -15,7 +19,7 @@ const ProductAll = () => {
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [query]);
 
   return (
     <div>
