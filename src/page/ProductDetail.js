44f6033866598df7ff1d2cf9e2e837dde9d71a7e
Juhwa Hwang <zerothhan@gmail.com>
2022-08-23 09:53:13 +0900

    10. 상품 상세 페이지 만들기

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-  return <div>상품 상세 페이지</div>;
+  let { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  const getProductDetail = async () => {
+    let url = `http://localhost:3004/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  };
+
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col className="product-img">
+          <img src={product?.img} />
+        </Col>
+        <Col>
+          <div>{product?.title}</div>
+          <div>{product?.price}</div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProductDetail;
