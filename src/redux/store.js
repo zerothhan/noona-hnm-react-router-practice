14b1a7c2aad16a077f34476e0077fd03c3cbdfea
Juhwa Hwang <zerothhan@gmail.com>
2022-08-24 11:42:27 +0900

    10. 리덕스 미들웨어 - 2. 리덕스 세팅

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import productReducer from "./reducers/productReducer";
+
+
+let store = createStore(productReducer, applyMiddleware(thunk));
+
+export default store;
